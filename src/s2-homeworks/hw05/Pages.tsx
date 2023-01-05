a26c2378d2ac76da62af7628e443ed1704ae106d
lgnvmaksim <lgnvmaksim@gmail.com>
2023-01-05 14:44:44 +0300

    HW-05 test push for git-hub deploy-02

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,11 +18,11 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route path={'/'} element={<PreJunior/>} />
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>} />
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                <Route element={<PreJunior/>} />
-                <Route element={<Junior/>} />
-                <Route element={<JuniorPlus/>} />
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>} />
+                <Route path={PATH.JUNIOR}  element={<Junior/>} />
+                <Route path={PATH.JUNIOR_PLUS}  element={<JuniorPlus/>} />
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
                 <Route path={'/*'} element={<Error404/>} />
